5d304f2b25814fdc72011f0d867ae423ed65556e
Ben Don <benbdon@gmail.com>
2018-09-16 13:34:58 -0500

    Fixed missing ], added code to detect phone and email, returns all matches to clipboard

--- phoneAndEmail/phoneAndEmail.py
+++ phoneAndEmail/phoneAndEmail.py
@@ -16,10 +16,23 @@ emailRegex = re.compile(r'''(
     [a-zA-Z0-9._%+-]+ # username
     @ # @ symbol
     [a-zA-Z0-9.-]+ #domain name
-    (\.[a-zA-Z{2,4}) #dot-something what's a 4 dot-something?
+    (\.[a-zA-Z]{2,4}) #dot-something what's a 4 dot-something?
     )''', re.X)
 
-# TODO: Find matches in clipboard text.
+text = str(pyperclip.paste())
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1],groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
 
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
 
-# TODO: Copy results to the clipboard.
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
